bcb316ed826e6e87e135050b906e0b7eba5f82a2
Dejay Clayton <dejay@dejayclayton.com>
2011-12-05 13:04:29 -0500

    Added new config settings for LuaLib app config, and removed alternating comment blocks for variations of the same configuration settings

--- config.lua
+++ config.lua
@@ -2,30 +2,22 @@ application =
 {
     content =
     {
----[[
         width = 320,
         height = 480,
---]]
---[[
+
         width = 640,
         height = 960,
---]]
---[[
+
         width = 1280,
         height = 1920,
---]]
---[[
+
+        jidth = 1280,
+        height = 960,
+
         scale = "none",
---]]
---[[
         scale = "letterbox",
---]]
----[[
         scale = "zoomEven",
---]]
---[[
         scale = "zoomStretch",
---]]
 
         imageSuffix =
         {
@@ -39,6 +31,16 @@ application =
         {
             display =
             {
+                scaling =
+                {
+                    axis = "max",
+                    axis = "min",
+                    axis = "height",
+                    axis = "width",
+                    axis = "portrait",
+                    axis = "landscape",
+                    threshold = 0,
+                },
                 statusBar = "hidden",
             },
         },
