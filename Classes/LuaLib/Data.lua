1422f70af86e0795e4e977e4fb1ed5314a11ef7c
Dejay Clayton <dejay@dejayclayton.com>
2011-12-07 09:32:34 -0500

    Added memoize function to 'Data.lua'

--- Classes/LuaLib/Data.lua
+++ Classes/LuaLib/Data.lua
@@ -21,4 +21,38 @@ function Data.selectByNestedIndex( targetTable, ... )
     return objRef
 end
 
+-- Thanks to http://lua-users.org/wiki/FuncTables
+function Data.memoize( fn, self )
+    fn = fn or function( x ) return nil end
+
+    if ( self == nil ) then
+    return setmetatable( {},
+    {
+        __index = function( table, key )
+            key = key or ""
+            local value = fn( key )
+            table[ key ] = value
+            return value
+        end,
+        __call  = function( table, key )
+            return table[ key ]
+        end
+    } )
+    end
+
+    return setmetatable( {},
+    {
+        __index = function( table, key )
+            key = key or ""
+            local value = fn( self, key )
+            table[ key ] = value
+            return value
+        end,
+        __call  = function( table, self, key )
+            key = key or ""
+            return table[ key ]
+        end
+    } )
+end
+
 return Data
