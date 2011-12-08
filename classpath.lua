fc582bc26260db7ee7d29dc525ed830216c60c86
Dejay Clayton <dejay@dejayclayton.com>
2011-12-08 17:27:30 -0500

    Moved 'classpath-SwapBlocks.lua' to 'Classes/SwapBlocks'

--- classpath.lua
+++ classpath.lua
@@ -3,5 +3,5 @@ OS_PATH_SEP = package.config:sub( 1, 1 )
 return {
     root = "Classes",
     LuaLib = require( "Classes/LuaLib/classpath-LuaLib" ),
-    SwapBlocks = require( "classpath-SwapBlocks" )
+    SwapBlocks = require( "Classes/SwapBlocks/classpath-SwapBlocks" )
 }
