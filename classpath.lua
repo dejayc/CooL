b489b2497ba26d69c313eea13b66b31fd469c19b
Dejay Clayton <dejay@dejayclayton.com>
2011-12-08 17:30:00 -0500

    Renamed 'classpath-LuaLib.lua' to 'classpath.lua'

--- classpath.lua
+++ classpath.lua
@@ -2,6 +2,6 @@ OS_PATH_SEP = package.config:sub( 1, 1 )
 
 return {
     root = "Classes",
-    LuaLib = require( "Classes/LuaLib/classpath-LuaLib" ),
+    LuaLib = require( "Classes/LuaLib/classpath" ),
     SwapBlocks = require( "Classes/SwapBlocks/classpath-SwapBlocks" )
 }
