b8a2959d2c61b3cfeeb4281e28a979d8b9211314
Dejay Clayton <dejay@dejayclayton.com>
2011-12-08 22:01:42 -0500

    Updated 'main.lua' to have some useful print statements for beginners

--- Examples/BasicApp/main.lua
+++ Examples/BasicApp/main.lua
@@ -1,8 +1,18 @@
 local CLASSPATH = require( "classpath" )
 
+print [[
+Please make sure that 'config.lua' is symlinked or copied to
+'config-local.lua'!
+]]
+
 local Config = require( CLASSPATH.CooL.Config )
 local config = Config:new()
 config:init( require( "config-local" ) )
+
+print [[
+This basic app isn't very visual, but debugging print statements will reveal
+screen statistics when you rotate the hardware in the Corona simulator.
+]]
 
 local App = require( CLASSPATH.CooL.App )
 local app = App:new()
