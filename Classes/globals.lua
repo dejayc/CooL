d1c09a38ed19c2dfc6444ed106ea5477e42328b0
Dejay Clayton <dejay@dejayclayton.com>
2011-12-15 11:23:51 -0500

    Added 'globals.lua' to contain global functions native to CooL

---
+++ Classes/globals.lua
@@ -0,0 +1,20 @@
+--[[ CooL( Corona Object-Oriented Lua )
+     https://github.com/dejayc/CooL
+     Copyright 2011 Dejay Clayton
+
+     All use of this file must comply with the Apache License,
+     Version 2.0, under which this file is licensed:
+
+     http://www.apache.org/licenses/LICENSE-2.0 --]]
+
+function cast( name, ... )
+    return require( name ):new( ... )
+end
+
+function new( name, ... )
+    return require( name ):new( nil, ... )
+end
+
+function extend( name, ... )
+    return require( name ):extend( ... )
+end
