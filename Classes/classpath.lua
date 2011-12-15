9cecdb7c19a0b61cfa6c7a788b7a6c8213de19df
Dejay Clayton <dejay@dejayclayton.com>
2011-12-15 11:35:30 -0500

    Added classpaths for 'Config' and 'FrameworkConfig', and 'required' the new 'globals.lua'

--- Classes/classpath.lua
+++ Classes/classpath.lua
@@ -7,14 +7,18 @@
 
      http://www.apache.org/licenses/LICENSE-2.0 --]]
 
-local _, _, INCLUDE_PATH = string.find( ..., "^(.*%.).-$" )
+local _, _, INCLUDE_PATH = string.find( arg[ 1], "^(.*%.).-$" )
 if ( INCLUDE_PATH == nil ) then INCLUDE_PATH = "" end
+
+require( INCLUDE_PATH .. "globals" )
 
 return {
     App = INCLUDE_PATH .. "App",
     Class = INCLUDE_PATH .. "Class",
+    Config = INCLUDE_PATH .. "Config",
     Data = INCLUDE_PATH .. "Data",
     Display = INCLUDE_PATH .. "Display",
     File = INCLUDE_PATH .. "File",
+    FrameworkConfig = INCLUDE_PATH .. "FrameworkConfig",
     PlatformConfig = INCLUDE_PATH .. "PlatformConfig",
 }
