99d90b149e7ca31f325cbc9116a532a2ba2f4a8c
Dejay Clayton <dejay@dejayclayton.com>
2011-12-15 12:19:29 -0500

    Changed some global constant names in 'classpath.lua'

--- Classes/classpath.lua
+++ Classes/classpath.lua
@@ -7,18 +7,22 @@
 
      http://www.apache.org/licenses/LICENSE-2.0 --]]
 
-local _, _, INCLUDE_PATH = string.find( ..., "^(.*%.).-$" )
-if ( INCLUDE_PATH == nil ) then INCLUDE_PATH = "" end
+local _, _, THIS_PACKAGE_PREFIX = string.find( ..., "^(.*%.).-$" )
+if ( THIS_PACKAGE_PREFIX == nil ) then THIS_PACKAGE_PREFIX = "" end
 
-require( INCLUDE_PATH .. "globals" )
+-- Globals
+COOL_PACKAGE_PREFIX = THIS_PACKAGE_PREFIX
+COOL_CLASS_PACKAGE = COOL_PACKAGE_PREFIX .. "Class"
+require( THIS_PACKAGE_PREFIX .. "globals" )
 
+-- CooL Classpaths
 return {
-    App = INCLUDE_PATH .. "App",
-    Class = INCLUDE_PATH .. "Class",
-    Config = INCLUDE_PATH .. "Config",
-    Data = INCLUDE_PATH .. "Data",
-    Display = INCLUDE_PATH .. "Display",
-    File = INCLUDE_PATH .. "File",
-    FrameworkConfig = INCLUDE_PATH .. "FrameworkConfig",
-    PlatformConfig = INCLUDE_PATH .. "PlatformConfig",
+    App = THIS_PACKAGE_PREFIX .. "App",
+    Class = COOL_CLASS_PACKAGE,
+    Config = THIS_PACKAGE_PREFIX .. "Config",
+    Data = THIS_PACKAGE_PREFIX .. "Data",
+    Display = THIS_PACKAGE_PREFIX .. "Display",
+    File = THIS_PACKAGE_PREFIX .. "File",
+    FrameworkConfig = THIS_PACKAGE_PREFIX .. "FrameworkConfig",
+    PlatformConfig = THIS_PACKAGE_PREFIX .. "PlatformConfig",
 }
