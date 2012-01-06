cacad9f8f76c3a151b914d4b3a5d13c4a532af32
Dejay Clayton <dejay@dejayclayton.com>
2012-01-06 13:48:01 -0500

    Added method 'File.getRequirePath'

--- Classes/File.lua
+++ Classes/File.lua
@@ -8,19 +8,51 @@
      http://www.apache.org/licenses/LICENSE-2.0 --]]
 
 local CLASSPATH = require( "classpath" )
+local Data = require( CLASSPATH.CooL.Data )
 
 local CLASS = {}
 
+CLASS.CLASS_SEPARATOR = '.'
+CLASS.LUA_FILE_EXTENSION = ".lua"
 CLASS.PATH_SEPARATOR = package.config:sub( 1, 1 )
+CLASS.PATH_TRIM_PATTERN = string.format(
+    "^[%s]?(.-)[%s]?$", CLASS.PATH_SEPARATOR, CLASS.PATH_SEPARATOR )
 
-function CLASS.fileExists( filename, coronaPathType )
-  return CLASS.getFilePath( filename, coronaPathType ) ~= nil
+function CLASS.fileExists( fileName, coronaPathType )
+  return CLASS.getFilePath( fileName, coronaPathType ) ~= nil
 end
 
 -- Thanks to http://bsharpe.com/code/coronasdk-how-to-know-if-a-file-exists/
-function CLASS.getFilePath( filename, coronaPathType )
-  local filePath = system.pathForFile( filename, coronaPathType )
+function CLASS.getFilePath( fileName, coronaPathType )
+  local filePath = system.pathForFile( fileName, coronaPathType )
   return filePath
+end
+
+function CLASS.getRequirePath( filePath, fileName )
+    if ( Data.endsWith( fileName, CLASS.LUA_FILE_EXTENSION ) ) then
+        fileName = string.sub(
+            fileName, 1, -1 * ( string.len( CLASS.LUA_FILE_EXTENSION ) + 1 ) )
+    end
+
+    if ( filePath == nil or filePath == "" or
+         filePath == CLASS.PATH_SEPARATOR )
+    then
+        return fileName
+    end
+
+    filePath = string.gsub(
+        string.match( filePath, CLASS.PATH_TRIM_PATTERN ),
+        CLASS.PATH_SEPARATOR, CLASS.CLASS_SEPARATOR )
+
+    if ( fileName == nil or fileName == "" ) then
+        return filePath
+    end
+
+    if ( filePath == "" ) then
+        return fileName
+    end
+
+    return filePath .. CLASS.CLASS_SEPARATOR .. fileName
 end
 
 return CLASS
