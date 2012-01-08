6f233994dcdd0e82d5c039e8a656b724b0c456c4
Dejay Clayton <dejay@dejayclayton.com>
2012-01-07 20:26:26 -0500

    Continuing implementation of 'SpriteSheet'

--- Classes/SpriteSheet.lua
+++ Classes/SpriteSheet.lua
@@ -8,8 +8,8 @@
      http://www.apache.org/licenses/LICENSE-2.0 --]]
 
 local CLASSPATH = require( "classpath" )
-local Data = require( CLASSPATH.CooL.Data )
 local File = require( CLASSPATH.CooL.File )
+local sprite = require( "sprite" )
 
 local CLASS = autoclass( packagePath( ... ) )
 
@@ -33,35 +33,6 @@ function CLASS:getSpriteSheetData()
     return self.spriteSheetData
 end
 
-function CLASS:prepare( dataFileName, imageFileName, coronaPathType )
-    self:release()
-
-    self.dataFileName = dataFileName
-    self.imageFileName = imageFileName
-    self.coronaPathType = coronaPathType
-
-    local spriteSheetData = require( Data.getRequirePath( dataFileName ) ).
-        spriteSheetMethod.getSpriteSheetData()
-    local spriteNameLookup = {}
-
-    for index, value in ipairs( spriteSheetData.sheet.frames ) do
-        spriteNameLookup[ value.name ] = index
-    end
-
-    self.spriteSheetData = spriteSheetData
-    self.spriteNameLookup = spriteNameLookup
-end
-
-function CLASS:load()
-    if ( spriteSheet == nil ) then
-        self:release()
-        self.spriteSheet = sprite.newSpriteSheetFromData(
-            self.imageFileName, self.spriteSheetData, self.coronaPathType )
-        self.spriteSet = sprite.newSpriteSet(
-            self.spriteSheet, 1, table.getn( self.spriteNameLookup ) )
-    end
-end
-
 function CLASS:release()
     if ( self.spriteSheet ~= nil ) then
         self.spriteSheet:dispose()
@@ -72,8 +43,46 @@ function CLASS:release()
     end
 end
 
-function CLASS:getSprite( spriteName )
-    local spriteIndex = self.spriteNameLookup[ spriteName ]
+function CLASS:prepare( dataFileName, imageFileName, coronaPathType )
+    self:release()
+
+    self.dataFileName = dataFileName
+    self.imageFileName = imageFileName
+    self.coronaPathType = coronaPathType
+
+    local spriteSheetData = require( File.getRequirePath( dataFileName ) ).
+        getSpriteSheetData()
+
+    local spriteNames = {}
+    local spriteIndexLookup = {}
+
+    for index, value in ipairs( spriteSheetData.frames ) do
+        spriteNames[ index ] = value.name
+        spriteIndexLookup[ value.name ] = index
+    end
+
+    self.spriteSheetData = spriteSheetData
+    self.spriteNames = spriteNames
+    self.spriteIndexLookup = spriteIndexLookup
+end
+
+function CLASS:load()
+    if ( spriteSheet == nil ) then
+        self:release()
+
+        self.spriteSheet = sprite.newSpriteSheetFromData(
+            self.imageFileName, self.spriteSheetData, self.coronaPathType )
+
+        self.spriteSet = sprite.newSpriteSet(
+            self.spriteSheet, 1, table.getn( self.spriteNames ) )
+    end
+end
+
+function CLASS:getSprite( spriteIndex )
+    if ( type( spriteIndex ) == "string" ) then
+        spriteIndex = self.spriteIndexLookup[ spriteIndex ]
+    end
+
     if ( spriteIndex == nil ) then return nil end
 
     local thisSprite = sprite.newSprite( self.spriteSet )
